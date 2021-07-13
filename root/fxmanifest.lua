7c127c1828b6673a2e77f5332e2d2ae8f547b552
Caydos <63801222+Caydos@users.noreply.github.com>
2021-07-13 12:11:32 +0200

    Add files via upload

---
+++ root/fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'adamant'
+game 'gta5'
+
+-- client_scripts  {"lua/cl.lua"}
+
+
+
+ui_page "index.html"
+
+files {
+    "index.html",
+    "img/*",
+    "script.js",
+    "jquery.js",
+    "style.css",
+    "fonts/*"
+}
+
